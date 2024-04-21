672b646470913b875546103ccd0922e76e94fe56
Fazle hasan <70137126+Fazle-hasan@users.noreply.github.com>
2024-04-22 00:25:00 +0530

    adding all new fiels

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+
+app = Flask(__name__)
+app.secret_key = '4f2bfa6592418c6a7e50573998ce99db'
+
+# Mock database for user credentials
+users = {'fazle': '123456', 'gauri': '789100'}
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username in users and users[username] == password:
+            session['username'] = username
+            return redirect(url_for('index'))
+        else:
+            return render_template('login.html', error='Invalid username or password')
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('login'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
